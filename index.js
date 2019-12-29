e0881788f39bfbfd93facb56e6a2cd37f5f3372b
ron erlih <ronerlih@gmail.com>
2019-12-29 17:50:10 -0500

    cluster

--- index.js
+++ index.js
@@ -6,4 +6,26 @@ require('@babel/register')({
   plugins: ['@babel/plugin-proposal-class-properties']
 })
 console.log('\n\n\n\n\n--index js entry');
-require('./server')
+
+// cluster 
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  // Workers can share any TCP connection
+  // In this case it is an HTTP server
+  require('./server')
+ 
+  console.log(`Worker ${process.pid} started`);
+}
